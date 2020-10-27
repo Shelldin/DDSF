f4f85e71219b3cf9693c0a4ed01e13070c4a307d
Sheldon Bean <sheldon.bean@gmail.com>
2020-10-27 01:47:10 -0600

    DiceRoller and DiceSO

---
+++ DDSF/Assets/Scripts/DiceRoller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoller : MonoBehaviour
+{
+    public DiceIntSO diceInfo;
+    private int rollResult,
+        modifier,
+        totalResult;
+
+    private string resultMessage;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rollResult = Random.Range(1, diceInfo.sideInt+1);
+            modifier = diceInfo.modifierInt;
+            totalResult = rollResult + modifier;
+            resultMessage = rollResult.ToString() + " + " + modifier.ToString() + " = " + totalResult.ToString();
+            Debug.Log(resultMessage);
+        }  
+    }
+
+    public void Roll()
+    {
+       
+    }
+        
+}
---
+++ DDSF/Assets/Scripts/ScriptableObjects/DiceIntSO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class DiceIntSO : ScriptableObject
+{
+    public int diceInt,
+        sideInt,
+        modifierInt;
+}
