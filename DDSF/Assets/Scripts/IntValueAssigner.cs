29d1697a790359a67a15fca3534f1a1c8d2abc3c
Sheldon Bean <sheldon.bean@gmail.com>
2020-11-01 23:58:17 -0700

    Created IntValueAssigner

---
+++ DDSF/Assets/Scripts/IntValueAssigner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntValueAssigner : MonoBehaviour
+{
+   public DiceIntSO diceInfo;
+   public InputField inputField;
+   [Range(1,100)]
+   public int diceIntRange,
+      sideIntRange,
+      modifierIntRange;
+
+   private void Awake()
+   {
+      inputField.onEndEdit.AddListener(ReceiveStringValueInput);
+   }
+
+   private void ReceiveStringValueInput(string playerInput)
+   {
+      int.TryParse(playerInput, out diceIntRange);
+      Debug.Log(diceIntRange);
+   }
+}
