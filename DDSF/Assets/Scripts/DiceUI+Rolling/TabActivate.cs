2bdc5ebe6f3b0ead3a05439173cb0acad36fbbe4
Sheldon Bean <sheldon.bean@gmail.com>
2020-11-12 23:58:18 -0700

    Created tabactivate to change active inputfield with tab key

---
+++ DDSF/Assets/Scripts/DiceUI+Rolling/TabActivate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabActivate : MonoBehaviour
+{
+    public InputField [] inputFields;
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown("tab") && inputFields[1].IsActive())
+        {
+            inputFields[2].ActivateInputField();
+        }
+        
+        if (Input.GetKeyDown("tab") && inputFields[0].IsActive())
+        {
+            inputFields[1].ActivateInputField();
+        }
+        
+        
+    }
+}
