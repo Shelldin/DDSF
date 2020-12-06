938cbd6fef21d0e7a1140299cac977dc379d2aa4
Sheldon Bean <sheldon.bean@gmail.com>
2020-12-06 05:31:30 -0700

    Creating UnityEvents and CharacterClassSelect function

---
+++ DDSF/Assets/Scripts/CharacterSheet/CharacterSheetMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class CharacterSheetMenu : MonoBehaviour
+{
+    /* CharacterClassIndex Key:
+     * 0 = Select Class...
+     * 1 = Artificer
+     * 2 = Barbarian
+     * 3 = Bard
+     * 4 = Cleric
+     * 5 = Druid
+     * 6 = Fighter
+     * 7 = Monk
+     * 8 = Paladin
+     * 9 = Ranger
+     * 10 = Rogue
+     * 11 = Sorcerer
+     * 12 = Warlock
+     * 13 = Wizard
+     */
+    public UnityEvent ClassMenuEvent,
+        artificerMenuEvent,
+        barbarianMenuEvent,
+        bardMenuEvent,
+        clericMenuEvent,
+        druidMenuEvent,
+        fighterMenuEvent,
+        monkMenuEvent,
+        paladinMenuEvent,
+        rangerMenuEvent,
+        rogueMenuEvent,
+        sorcererMenuEvent,
+        warlockMenuEvent,
+        wizardMenuEventEvent;
+
+
+    private void Start()
+    {
+    }
+
+    public void SelectCharacterClass(int characterClassIndex)
+    {
+        if (characterClassIndex == 0)
+        {
+            ClassMenuEvent.Invoke();
+        }
+
+        if (characterClassIndex == 1)
+        {
+            artificerMenuEvent.Invoke();
+        }
+    }
+}
---
+++ DDSF/Assets/Scripts/ScriptableObjects/CharacterSheetSO.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class CharacterSheetSO : ScriptableObject
+{
+    public string characterName;
+
+    public int level,
+        experience,
+        proficiencyBonus,
+        str,
+        dex,
+        con,
+        intel,
+        wis,
+        cha,
+        hitPointMax,
+        currentHitPoints,
+        tempHitPoints,
+        armorClass,
+        walkSpeed,
+        walkSpeedSpace,
+        swimSpeed,
+        swimSpeedSpace,
+        flySpeed,
+        flySpeedSpace;
+    
+    public Dropdown characterClass,
+        characterSubClass,
+        characterRace,
+        characterSubRace;
+}
