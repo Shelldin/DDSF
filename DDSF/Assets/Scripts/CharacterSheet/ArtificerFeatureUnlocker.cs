b3086f061c4e72f949676d45b7a434323db2cf8d
Sheldon Bean <sheldon.bean@gmail.com>
2020-12-12 07:12:08 -0700

    Added Some scripts to unlock level features for some classes

---
+++ DDSF/Assets/Scripts/CharacterSheet/ArtificerFeatureUnlocker.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArtificerFeatureUnlocker : MonoBehaviour
+{
+    public CharacterSheetSO charSheet;
+    public InputField expInputField;
+    public Dropdown subClassDropdown;
+
+    private void Start()
+    {
+        expInputField.onEndEdit.AddListener(UnlockFeaturesOnLevelUp);
+    }
+
+    private void UnlockFeaturesOnLevelUp(string playerInput)
+    {
+        UnlockSubClassDropdown();
+    }
+
+    private void UnlockSubClassDropdown()
+    {
+        if (charSheet.level>=3 && charSheet.classMenuDropdownValue ==1)
+        {
+            subClassDropdown.interactable = true;
+        }
+        else
+        { 
+            subClassDropdown.interactable = false;
+        }
+    }
+    
+}
---
+++ DDSF/Assets/Scripts/CharacterSheet/BarbarianFeatureUnlocker.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarbarianFeatureUnlocker : MonoBehaviour
+{
+    
+    public CharacterSheetSO charSheet;
+    public InputField expInputField;
+    public Dropdown subClassDropdown;
+    
+    private void Start()
+    {
+        expInputField.onEndEdit.AddListener(UnlockFeaturesOnLevelUp);
+    }
+
+    private void UnlockFeaturesOnLevelUp(string playerInput)
+    {
+        UnlockSubClassDropdown();
+    }
+
+    private void UnlockSubClassDropdown()
+    {
+        if (charSheet.level>=3 && charSheet.classMenuDropdownValue ==2)
+        {
+            subClassDropdown.interactable = true;
+        }
+        else
+        { 
+            subClassDropdown.interactable = false;
+        }
+    }
+
+    
+}
