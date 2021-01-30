8a42922071b6b5b551417768fb9c2d55d8ec32aa
Sheldon Bean <sheldon.bean@gmail.com>
2021-01-29 23:57:26 -0700

    inputtextassigner script

---
+++ DDSF/Assets/Scripts/CharacterSheet/InputTextAssinger.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class InputTextAssinger : MonoBehaviour
+{
+    public CharacterSheetSO charSheet;
+    public InputField inputField;
+    public UnityEvent unityEvent;
+    
+        void Start()
+    {
+        inputField.onEndEdit.AddListener(OnEndInvoke);
+    }
+
+        private void OnEndInvoke(string playerInput)
+        {
+            unityEvent.Invoke();
+        }
+        
+        
+}
