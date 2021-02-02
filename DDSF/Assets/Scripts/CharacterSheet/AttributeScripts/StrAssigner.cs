d3437673c8946c857450588991c9b1d9d8a91a8a
Sheldon Bean <sheldon.bean@gmail.com>
2021-02-01 23:43:35 -0700

    StrAssigner Script and ability scores UI

---
+++ DDSF/Assets/Scripts/CharacterSheet/AttributeScripts/StrAssigner.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StrAssigner : MonoBehaviour
+{
+    public CharacterSheetSO charSheet;
+    public InputField inputField;
+    public Text modifierText;
+
+    private int modifierInt;
+    private float modifierFloat;
+
+    void Start()
+    {
+        inputField.onEndEdit.AddListener(AssignStr);
+    }
+
+    private void AssignStr(string playerInput)
+    {
+        charSheet.str = int.Parse(playerInput);
+        if (charSheet.str <1)
+        {
+            charSheet.str = 1;
+            inputField.text = charSheet.str.ToString();
+        }
+
+        modifierFloat = Mathf.Floor((charSheet.str - 10) / 2);
+        Debug.Log(modifierFloat.ToString());
+    }
+}
